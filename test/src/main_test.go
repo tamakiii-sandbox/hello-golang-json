f9c7f59fa50464b6d6c95a562c98ea16f90d2f84
tamakiii <tamakiii@users.noreply.github.com>
2020-11-19 19:47:17 +0900

    fm~~~

--- test/src/main_test.go
+++ test/src/main_test.go
@@ -7,14 +7,14 @@ import (
 )
 
 func TestLoadUsersFile(t *testing.T) {
-  _, error := ioutil.ReadFile("../../test/users.json")
+  _, error := ioutil.ReadFile("../users.json")
   if error != nil {
     t.Fatalf("failed to load users.json")
   }
 }
 
 func TestLoadOrganizationsFile(t *testing.T) {
-  _, error := ioutil.ReadFile("../../test/organizations.json")
+  _, error := ioutil.ReadFile("../organizations.json")
   if error != nil {
     t.Fatalf("failed to load users.json")
   }
